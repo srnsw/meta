92f8fbb9d85546e0def225855aadd9b8321d4a84
Glen Humphries <glen.humphries@records.nsw.gov.au>
2017-04-13 16:21:23 +1000

    couple more changes to meta

--- metadata.go
+++ metadata.go
@@ -2,13 +2,13 @@ package meta
 
 // represents a metadata.json file
 type Metadata struct {
-	Title        string       `json:"title"`
-	Created      string       `json:"created,omitempty"`
-	Creators     []Agent      `json:"creators,omitempty"`
-	Series       int          `json:"series"`
-	DisposalRule DisposalRule `json:"disposalRule"`
-	Audio        MediaFields  `json:"audio,omitempty"`
-	Video        MediaFields  `json:"video,omitempty"`
+	Title        string         `json:"dct:title"`
+	Created      string         `json:"created,omitempty"`
+	Creators     []Agent        `json:"creators,omitempty"`
+	Series       int            `json:"series"`
+	DisposalRule []DisposalRule `json:"disposalRules"`
+	Audio        MediaFields    `json:"audio,omitempty"`
+	Video        MediaFields    `json:"video,omitempty"`
 }
 
 type MediaFields struct {
